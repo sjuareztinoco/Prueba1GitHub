a751d92b4ae8567dc5ed8343e82f97d7f772cdc9
Senen Juarez Tinoco <senen.juarez@gmail.com>
2023-04-19 14:35:19 -0600

    primer cambio ya como dev1
    SJT_dev1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
+        System.out.println("Todo en orden dev1");
     }
 }
